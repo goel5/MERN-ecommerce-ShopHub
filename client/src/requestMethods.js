7c39b4d0fe2d83153c30a034c5eb8bf1b4017bbe
goel5 <sajalgoyal5@gmail.com>
2021-10-06 10:12:54 +0530

    test

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = '/api/';
 const TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWE4OTMzN2E4Y2E1YmI3NDQ3NzQxYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzQxNDkzNywiZXhwIjoxNjMzNjc0MTM3fQ._5Uu_c0zB6UzxKA5-2Mw0oZDNawn3JLkOM6e591OOz0';
 
