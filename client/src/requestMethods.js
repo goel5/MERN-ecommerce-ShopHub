232273a48fc80f05878408b7e48b66159dcc5212
goel5 <sajalgoyal5@gmail.com>
2021-10-06 15:55:19 +0530

    cleaning

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -2,20 +2,20 @@ import axios from 'axios';
 
 const BASE_URL = '/api/';
 
-// const liveUser = JSON.parse(
-//   JSON.parse(localStorage.getItem('persist:root')).user
-// ).currentUser;
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
-  .currentUser
-  ? JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
-      .currentUser.accessToken
-  : '';
-// let TOKEN = '';
-// console.log('ds', liveUser);
-// if (liveUser !== null) {
-//   TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
-//     .currentUser.accessToken;
-// }
+const liveUser = JSON.parse(
+  JSON.parse(localStorage.getItem('persist:root')).user
+).currentUser;
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
+//   .currentUser
+//   ? JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
+//       .currentUser.accessToken
+//   : '';
+let TOKEN = '';
+console.log('ds', liveUser);
+if (liveUser !== null) {
+  TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
+    .currentUser.accessToken;
+}
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
